8ba54b57e002d1c001f88e2ae5bb194399a3565e
Yury Bikuzin <yury.bikuzin@gmail.com>
2020-07-05 13:50:01 +0300

    json implemented

--- src/json/src/lib.rs
+++ src/json/src/lib.rs
@@ -4,6 +4,10 @@ use log::{error, warn, info, debug, trace};
 use anyhow::{anyhow, bail, Result, Error, Context};
 
 use std::fmt;
+use serde_json::{Value, Map};
+use tokio::fs::File;
+use tokio::prelude::*;
+use std::convert::TryFrom;
 
 // ============================================================================
 // ============================================================================
@@ -23,13 +27,6 @@ impl By {
     }
 }
 
-// impl AsRef<&By> for By {
-//     #[inline]
-//     fn as_ref(&self) -> &By {
-//         &self
-//     }
-// }
-
 impl fmt::Display for By {
     fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
         match self {
@@ -43,6 +40,8 @@ impl fmt::Display for By {
         Ok(())
     }
 }
+
+// ============================================================================
 
 #[derive(Debug, Clone)]
 pub enum JsonSource {
@@ -63,6 +62,8 @@ impl fmt::Display for JsonSource {
         Ok(())
     }
 }
+
+// ============================================================================
 
 #[derive(Debug, Clone)]
 pub struct JsonPath {
@@ -95,51 +96,14 @@ impl fmt::Display for JsonPath {
     }
 }
 
-use serde_json::{Value, Map, Number};
+// ============================================================================
+
 #[derive(Clone, Debug)]
 pub struct Json {
     pub value: Value,
     pub path: JsonPath,
 }
 
-use tokio::fs::File;
-use tokio::prelude::*;
-
-// macro_rules! as_64 {
-//     ($type: ty) => {
-//         paste::item!{
-//             pub fn [< as_ $type >] (&self) -> Result<u64> {
-//                 match &self.value {
-//                     Value::Number(n) => {
-//                         if n.[< is_ $type >] () {
-//                             let n = n.[< as_ $type >]().unwrap();
-//                             Ok(n)
-//                         } else {
-//                             bail!("{} expected to be a {}, but: {}", self.path, stringify!($type), n);
-//                         }
-//                     },
-//                     _ => {
-//                         bail!("{} expected to be a {}, but: {}", self.path, stringify!($type), serde_json::to_string_pretty(&self.value)?);
-//                     },
-//                 }
-//             }
-//         }
-//     }
-// }
-//
-// macro_rules! as_u {
-//     ($type: ty) => {
-//         paste::item!{
-//             pub fn [< as_ $type >] (&self) -> Result<$type> {
-//                 let n = self.as_u64()?;
-//                 let n = $type::try_from(n)
-//                     .map_err(|err| Error::new(err))
-//                     .context(format!("{} expected to be a {}, but: {}", self.path, stringify!($type), n))?;
-//                 Ok(n)
-//             }
-//         }
-//     }
-// }
 
 macro_rules! as_ {
     ($type: ty) => {
@@ -171,11 +135,60 @@ macro_rules! as_ {
                 Ok(n)
             }
         }
-    }
+    };
 }
 
+macro_rules! parse_as_ {
+    ($type: ty) => {
+        paste::item!{
+            pub fn [< parse_as_ $type >] (&self) -> Result<$type> {
+                match &self.value {
+                    Value::Number(n) => {
+                        if n.[< is_ $type >] () {
+                            let n = n.[< as_ $type >]().unwrap();
+                            Ok(n)
+                        } else {
+                            bail!("{} expected to be a {}, but: {}", self.path, stringify!($type), n);
+                        }
+                    },
+                    Value::String(s) => {
+                        s.parse::<$type>().context(format!("{} expected to be a {} or String parseable to {}, but: {}", self.path, stringify!($type), stringify!($type), serde_json::to_string_pretty(&self.value)?))
+                    },
+                    _ => {
+                        bail!("{} expected to be a {} or String parseable to {}, but: {}", self.path, stringify!($type), stringify!($type), serde_json::to_string_pretty(&self.value)?);
+                    },
+                }
+            }
+        }
+    };
+    ($from: ty => $type: ty) => {
+        paste::item!{
+            pub fn [< parse_as_ $type >] (&self) -> Result<$type> {
+                match &self.value {
+                    Value::Number(n) => {
+                        if n.[< is_ $from >] () {
+                            let n = n.[< as_ $from >]().unwrap();
+                            let n = $type::try_from(n)
+                                .map_err(|err| Error::new(err))
+                                .context(format!("{} expected to be a {}, but: {}", self.path, stringify!($type), n))?;
+                            Ok(n)
+                        } else {
+                            bail!("{} expected to be a {}, but: {}", self.path, stringify!($type), n);
+                        }
+                    },
+                    Value::String(s) => {
+                        s.parse::<$type>().context(format!("{} expected to be a {} or String parseable to {}, but: {}", self.path, stringify!($type), stringify!($type), serde_json::to_string_pretty(&self.value)?))
+                    },
+                    _ => {
+                        bail!("{} expected to be a {} or String parseable to {}, but: {}", self.path, stringify!($type), stringify!($type), serde_json::to_string_pretty(&self.value)?);
+                    },
+                }
+            }
+        }
+    };
+}
 
-impl Json {
+impl<'a> Json {
     pub fn new(value: Value, src: JsonSource) -> Self {
         Self {
             value,
@@ -195,14 +208,14 @@ impl Json {
             path: JsonPath::new(JsonSource::FilePath(file_path.to_owned())),
         })
     }
-    pub fn get<P: AsRef<[By]>>(&self, path_items: P) -> Result<Self> {
+    pub fn get<'b, P: AsRef<[By]>>(&'b self, path_items: P) -> Result<Self> {
         let mut ret = self.clone();
         for path_item in path_items.as_ref().iter() {
             ret = ret.get_by_path_item(&path_item)?;
         }
         Ok(ret)
     }
-    fn get_by_path_item(&self, path_item: &By) -> Result<Self> {
+    fn get_by_path_item<'b>(&'b self, path_item: &By) -> Result<Self> {
         let get_ret = 
             match path_item {
                 By::Key(key) => {
@@ -222,7 +235,7 @@ impl Json {
         let path = self.path.add(path_item.to_owned());
         Ok(Self { value, path })
     }
-    pub fn as_str(&self) -> Result<&str> {
+    pub fn as_str<'b>(&'b self) -> Result<&'b str> {
         match &self.value {
             Value::String(s) => Ok(s),
             _ => {
@@ -262,20 +275,53 @@ impl Json {
             },
         }
     }
+    pub fn map_iter<'b>(&'b self) -> Result<MapIterator<'b>> {
+        let map = self.as_map()?;
+        let iter = MapIterator {
+            iter: map.iter(),
+            path: &self.path,
+        };
+        Ok(iter)
+    }
+    pub fn vec_iter<'b>(&'b self) -> Result<VecIterator<'b>> {
+        let vec = self.as_vec()?;
+        let iter = VecIterator {
+            iter: vec.iter(),
+            path: &self.path,
+            index: 0,
+        };
+        Ok(iter)
+    }
+
     as_!(u64);
     as_!(u64 => usize);
     as_!(u64 => u32);
     as_!(u64 => u16);
     as_!(u64 => u8);
+
     as_!(i64);
     as_!(i64 => isize);
     as_!(i64 => i32);
     as_!(i64 => i16);
     as_!(i64 => i8);
-    as_!(f64);
-}
 
-use std::convert::TryFrom;
+    as_!(f64);
+
+    parse_as_!(u64);
+    parse_as_!(u64 => usize);
+    parse_as_!(u64 => u32);
+    parse_as_!(u64 => u16);
+    parse_as_!(u64 => u8);
+
+    parse_as_!(i64);
+    parse_as_!(i64 => isize);
+    parse_as_!(i64 => i32);
+    parse_as_!(i64 => i16);
+    parse_as_!(i64 => i8);
+
+    parse_as_!(f64);
+
+}
 
 impl fmt::Display for Json {
     fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
@@ -285,6 +331,58 @@ impl fmt::Display for Json {
     }
 }
 
+// ============================================================================
+
+// #[derive(Debug)]
+pub struct MapIterator<'a> {
+    path: &'a JsonPath,
+    iter: serde_json::map::Iter<'a>,
+}
+
+impl<'a> Iterator for MapIterator<'a> {
+    type Item = (&'a str, Json);
+    fn next(&mut self) -> Option<Self::Item> {
+        let item = self.iter.next();
+        match item {
+            None => None,
+            Some((key, value)) => {
+                let key = key.as_ref();
+                let value = Json {
+                    value: value.clone(),
+                    path: self.path.add(By::key(key)),
+                };
+                Some((key, value))
+            }
+        }
+    }
+}
+
+// ============================================================================
+
+#[derive(Debug)]
+pub struct VecIterator<'a> {
+    path: &'a JsonPath,
+    iter: std::slice::Iter<'a, Value>,
+    index: usize,
+}
+
+impl<'a> Iterator for VecIterator<'a> {
+    type Item = Json;
+    fn next(&mut self) -> Option<Self::Item> {
+        let item = self.iter.next();
+        match item {
+            None => None,
+            Some(value) => {
+                let value = Json {
+                    value: value.clone(),
+                    path: self.path.add(By::index(self.index)),
+                };
+                self.index += 1;
+                Some(value)
+            }
+        }
+    }
+}
 
 // ============================================================================
 // ============================================================================
@@ -292,9 +390,6 @@ impl fmt::Display for Json {
 
 #[cfg(test)]
 mod tests {
-
-    // #[allow(unused_imports)]
-    // use pretty_assertions::{assert_eq, assert_ne};
     #[allow(unused_imports)]
     use log::{error, warn, info, debug, trace};
     use super::*;
@@ -304,8 +399,10 @@ mod tests {
         INIT.call_once(|| env_logger::init());
     }
 
+    use pretty_assertions::{assert_eq};
+
     #[tokio::test]
-    async fn test_json() -> Result<()> {
+    async fn test_json_0() -> Result<()> {
         init();
 
         let file_path = std::path::Path::new("test_data/sample.json");
@@ -337,12 +434,31 @@ mod tests {
         let tst = err.unwrap_err().downcast::<String>().unwrap();
         let eta = format!("{:?}.{:?} expected to be a String, but: {}", file_path, key, serde_json::to_string_pretty(&value.value)?);
         assert_eq!(tst, eta);
+        Ok(())
+    }
+
+    #[tokio::test]
+    async fn test_json_1() -> Result<()> {
+        init();
+
+        let file_path = std::path::Path::new("test_data/sample.json");
+        let json: Json = Json::from_file(&file_path).await?;
 
         let value = json.get([By::key("defaultModificationId")])?;
         let tst = format!("{}", value);
         let eta = r#""test_data/sample.json"."defaultModificationId": 363896"#;
         assert_eq!(tst, eta);
         
+        Ok(())
+    }
+        
+    #[tokio::test]
+    async fn test_json_2() -> Result<()> {
+        init();
+
+        let file_path = std::path::Path::new("test_data/sample.json");
+        let json: Json = Json::from_file(&file_path).await?;
+
         let key = "defaultModificationId2";
         let err = json.get([
             By::key(key)
@@ -350,26 +466,180 @@ mod tests {
         let tst = err.unwrap_err().downcast::<String>().unwrap();
         let eta = format!("{:?}: not found .{:?} at {}", file_path, key, serde_json::to_string_pretty(&json.value)?);
         assert_eq!(tst, eta); // assert_eq!(&err[0..80], &expected[0..80]);
-        
+
+        Ok(())
+    }
+
+    #[tokio::test]
+    async fn test_json_3() -> Result<()> {
+        init();
+
+        let file_path = std::path::Path::new("test_data/sample.json");
+        let json: Json = Json::from_file(&file_path).await?;
+
         let value = json.get([By::key("breadcrumbs"), By::index(1), By::key("title")])?;
         let tst = value.as_str()?;
         let eta = "Транспорт";
         assert_eq!(tst, eta);
 
+        Ok(())
+    }
+
+    #[tokio::test]
+    async fn test_json_4() -> Result<()> {
+        init();
+
+        let file_path = std::path::Path::new("test_data/sample.json");
+        let json: Json = Json::from_file(&file_path).await?;
+
         let value = json.get([By::key("breadcrumbs")])?;
         let _tst = value.as_vec()?;
 
+        Ok(())
+    }
+        
+    #[tokio::test]
+    async fn test_json_5() -> Result<()> {
+        init();
+
+        let file_path = std::path::Path::new("test_data/sample.json");
+        let json: Json = Json::from_file(&file_path).await?;
+
         let value = json.get([By::key("breadcrumbs"), By::index(1)])?;
         let _tst = value.as_map()?;
+
+        Ok(())
+    }
+
+    #[tokio::test]
+    async fn test_json_6() -> Result<()> {
+        init();
+
+        let file_path = std::path::Path::new("test_data/sample.json");
+        let json: Json = Json::from_file(&file_path).await?;
 
         let value = json.get([By::key("modifications"), By::key("items"), By::index(0), By::key("specification"), By::key("blocks"), By::index(0), By::key("params"), By::index(1), By::key("value")])?;
         let tst = value.as_str()?;
         assert_eq!(tst, "150");
 
-        // let _tst = value.as_map()?;
+        let tst = value.parse_as_u64()?;
+        assert_eq!(tst, 150);
+
+        let tst = value.parse_as_u8()?;
+        assert_eq!(tst, 150);
+
+        Ok(())
+    }
+        
+    #[tokio::test]
+    async fn test_json_7() -> Result<()> {
+        init();
+
+        let file_path = std::path::Path::new("test_data/sample.json");
+        let json: Json = Json::from_file(&file_path).await?;
+
+        let value = json.get([By::key("modifications"), By::key("items"), By::index(0), By::key("specification"), By::key("blocks"), By::index(2), By::key("params"), By::index(2), By::key("value")])?;
+
+        let tst = value.parse_as_u64()?;
+        assert_eq!(tst, 330);
+
+        let tst = value.parse_as_f64()?;
+        assert_eq!(tst as u64, 330);
+
+        let err = value.parse_as_u8();
+        let tst = err.unwrap_err().downcast::<String>().unwrap();
+        let eta = r#""test_data/sample.json"."modifications"."items"[0]."specification"."blocks"[2]."params"[2]."value" expected to be a u8 or String parseable to u8, but: "330""#.to_owned();
+        assert_eq!(tst, eta); 
 
         Ok(())
     }
 
+    #[tokio::test]
+    async fn test_json_8() -> Result<()> {
+        init();
+
+        let file_path = std::path::Path::new("test_data/sample.json");
+        let json: Json = Json::from_file(&file_path).await?;
+
+        let value = json.get([By::key("modifications"), By::key("items"), By::index(0), By::key("specification"), By::key("blocks"), By::index(0), By::key("params"), By::index(2), By::key("value")])?;
+        let tst = value.parse_as_f64()?;
+        assert_eq!(tst as u8, 2);
+
+        let err = value.parse_as_u8();
+        let tst = err.unwrap_err().downcast::<String>().unwrap();
+        let eta = r#""test_data/sample.json"."modifications"."items"[0]."specification"."blocks"[0]."params"[2]."value" expected to be a u8 or String parseable to u8, but: "2.0""#.to_owned();
+        assert_eq!(tst, eta); 
+
+        Ok(())
+    }
+
+    #[tokio::test]
+    async fn test_json_9() -> Result<()> {
+        init();
+
+        let file_path = std::path::Path::new("test_data/sample.json");
+        let json: Json = Json::from_file(&file_path).await?;
+
+        let value = json.get([By::key("breadcrumbs"), By::index(1), By::key("title")])?;
+        let err = value.parse_as_u64();
+        let tst = err.unwrap_err().downcast::<String>().unwrap();
+        let eta = r#""test_data/sample.json"."breadcrumbs"[1]."title" expected to be a u64 or String parseable to u64, but: "Транспорт""#.to_owned();
+        assert_eq!(tst, eta); 
+
+        Ok(())
+    }
+
+    #[tokio::test]
+    async fn test_json_a() -> Result<()> {
+        init();
+
+        let file_path = std::path::Path::new("test_data/sample.json");
+        let json: Json = Json::from_file(&file_path).await?;
+
+        let value = json.get([By::key("itemsBlockData"), By::key("searchButton")])?;
+        let mut keys: Vec<&str> = Vec::new();
+        for (key, val) in value.map_iter()? {
+            keys.push(key);
+            match key {
+                "errorInfoText" => {
+                    let bold = val.get([By::key("bold")])?;
+                    assert_eq!(bold.as_str()?, "На Авито нет BMW 5 серия");
+
+                    let err = bold.parse_as_u8();
+                    let tst = err.unwrap_err().downcast::<String>().unwrap();
+                    let eta = r#""test_data/sample.json"."itemsBlockData"."searchButton"."errorInfoText"."bold" expected to be a u8 or String parseable to u8, but: "На Авито нет BMW 5 серия""#.to_owned();
+                    assert_eq!(tst, eta); 
+                },
+                _ => {},
+            }
+        }
+        assert_eq!(keys, ["errorButtonText", "errorInfoText", "errorSearchLink", "infoText"]);
+
+        let err = value.vec_iter();
+        let tst = err.unwrap_err().downcast::<String>().unwrap();
+        let eta = "\"test_data/sample.json\".\"itemsBlockData\".\"searchButton\" expected to be an Array, but: {\n  \"errorButtonText\": \"Выбрать другой автомобиль на Авито\",\n  \"errorInfoText\": {\n    \"bold\": \"На Авито нет BMW 5 серия\",\n    \"normal\": \", но есть много других машин\"\n  },\n  \"errorSearchLink\": \"/rossiya/avtomobili\",\n  \"infoText\": {\n    \"bold\": {\n      \"few\": \"объявления BMW 5 серия\",\n      \"many\": \"объявлений BMW 5 серия\",\n      \"one\": \"объявление BMW 5 серия\",\n      \"other\": \"объявлений BMW 5 серия\"\n    },\n    \"normal\": \"найдено в России\"\n  }\n}".to_owned();
+        assert_eq!(tst, eta);
+
+        Ok(())
+    }
+
+    #[tokio::test]
+    async fn test_json_b() -> Result<()> {
+        init();
+
+        let file_path = std::path::Path::new("test_data/sample.json");
+        let json: Json = Json::from_file(&file_path).await?;
+
+        let value = json.get([By::key("breadcrumbs")])?;
+        let mut titles: Vec<String> = Vec::new();
+        for val in value.vec_iter()? {
+            let val = val.get([By::key("title")])?;
+            let s = val.as_str()?;
+            titles.push(s.to_owned());
+        }
+        assert_eq!(titles, ["Все объявления в России", "Транспорт", "Автомобили"]);
+
+        Ok(())
+    }
 }
 
